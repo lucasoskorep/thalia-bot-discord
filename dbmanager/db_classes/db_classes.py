a7fdca60436c7ba72e536e3d99dedf0aea12036f
Lucas Oskorep <lucas.oskorep@gmail.com>
2020-02-07 13:37:30 -0600

    Adding in dbmanager...jesus christ dude how did you forget to commit half a project

---
+++ dbmanager/db_classes/db_classes.py
@@ -0,0 +1,106 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.types import Text, String, BIGINT, DECIMAL
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    discord_id = Column(BIGINT, primary_key=True, nullable=False, unique=True)
+    name = Column(Text)
+
+    def __repr__(self):
+        return "<User(name='%s', dicord_id='%s')>" % (
+            self.name, self.discord_id)
+
+class Channel(Base):
+    __tablename__ = 'channel'
+
+    discord_id = Column(BIGINT, primary_key=True, nullable=False, unique=True)
+    name = Column(Text)
+
+    def __repr__(self):
+        return "<User(name='%s', dicord_id='%s')>" % (
+            self.name, self.discord_id)
+
+class Server(Base):
+    __tablename__ = 'server'
+
+    discord_id = Column(BIGINT, primary_key=True, nullable=False, unique=True)
+    name = Column(Text)
+
+    def __repr__(self):
+        return "<User(name='%s', dicord_id='%s')>" % (
+            self.name, self.discord_id)
+
+
+class ServerChannel(Base):
+    __tablename__ = 'server_channel'
+
+    id = Column(Integer, primary_key=True)
+
+    server_id = Column(BIGINT, ForeignKey('server.discord_id'), nullable=False)
+    channel_id = Column(BIGINT, ForeignKey('channel.discord_id'), nullable=False)
+
+
+class UserServer(Base):
+    __tablename__ = 'user_server'
+
+    id = Column(Integer, primary_key=True)
+
+    author_id = Column(BIGINT, ForeignKey('user.discord_id'), nullable=False)
+    server_id = Column(BIGINT, ForeignKey('server.discord_id'), nullable=False)
+    server = relationship('Server', foreign_keys=[server_id])
+    author = relationship('User', foreign_keys=[author_id])
+
+
+class UserToNames(Base):
+    __tablename__ = 'user_to_name'
+
+    id = Column(Integer, primary_key=True)
+    user = Column(BIGINT, ForeignKey('user.discord_id'), nullable=False)
+    name = Column(String(250), nullable=False)
+
+
+class Message(Base):
+    __tablename__ = 'messages'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+
+    content = Column(Text, nullable=False)
+    timestamp = Column(DECIMAL, nullable=False)
+    message_id = Column(BIGINT, unique=True, nullable=False, primary_key=True)
+    author_id = Column(BIGINT, ForeignKey('user.discord_id'), nullable=False, index=True)
+    server_id = Column(BIGINT, ForeignKey('server.discord_id'), nullable=False, index = True)
+    channel_id = Column(BIGINT, ForeignKey('channel.discord_id'), nullable=False, index=True, primary_key=True)
+    server = relationship('Server', foreign_keys=[server_id])
+    author = relationship('User', foreign_keys=[author_id])
+    channel = relationship('Channel', foreign_keys=[channel_id])
+
+    def __repr__(self):
+        return "<Message(content='%s', messsage_id='%s', author_id='%s', server_id='%s', channel_id='%s')>" % (
+            self.content, self.message_id, self.author_id, self.server_id, self.channel_id)
+
+# Create an engine that stores data in the local directory's
+# sqlalchemy_example.db file.
+
+# Grab the password from the environment
+# Grab the username from the environment
+# Grab the address from the environment
+client_key = os.getenv('DISCORD_CLIENT_KEY', '0')
+username = os.getenv('MYSQL_USERNAME', 'root')
+password = os.getenv('MYSQL_PASSWORD', 'password')
+address = os.getenv('MYSQL_ADDRESS', 'localhost:3306')
+discord_db = os.getenv('MYSQL_DISCORD_DB', 'THALIA')
+engine = create_engine('mysql+pymysql://' + username +':' + password +'@'+address+'/'+discord_db)
+#
+# # Create all tables in the engine. This is equivalent to "Create Table"
+# # statements in raw SQL.
+Base.metadata.create_all(engine)
---
+++ dbmanager/dbmanager.py
@@ -0,0 +1,230 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+from dbmanager.db_classes.db_classes import User, Server, Channel, Message
+import threading
+
+lock = threading.Lock()
+
+class dbmanager(object):
+    """
+
+    """
+
+    def __init__(self, username, password, db_location, db_name):
+        """
+
+        :param username:
+        :param password:
+        :param db_location:
+        :param db_name:
+        """
+        engine = create_engine('mysql+pymysql://' + username +':' + password +'@'+db_location+'/'+db_name)
+        self.session_factory = sessionmaker(bind=engine)
+        global Session
+        Session = scoped_session(self.session_factory)
+
+    def create_test_setup(self):
+        """
+
+        :return:
+        """
+        global Session
+        sess = Session()
+
+        new_person = User(discord_id=69, name="user")
+
+        sess.add(new_person)
+        sess.commit()
+
+        new_server = Server(discord_id=6969, name="server")
+        sess.add(new_server)
+        sess.commit()
+
+        new_channel = Channel(discord_id=696969, name="channel")
+        sess.add(new_channel)
+        sess.commit()
+
+        new_message = Message(
+            content="test content",
+            message_id = 12312312,
+            author_id = 69,
+            server_id = 6969,
+            channel_id = 696969
+        )
+        sess.add(new_message)
+        sess.commit()
+
+        Session.remove()
+
+    def create_message(self, content, timestamp, message_id, author_id, server_id, channel_id):
+        """
+
+        :param content:
+        :param message_id:
+        :param author_id:
+        :param server_id:
+        :param channel_id:
+        :return:
+        """
+        global Session
+        sess = Session()
+
+        new_message = Message(
+            content = content,
+            timestamp = timestamp,
+            message_id = message_id,
+            author_id = author_id,
+            server_id = server_id,
+            channel_id = channel_id
+        )
+        sess.add(new_message)
+        sess.commit()
+
+        Session.remove()
+
+    def create_messages(self, messages):
+        with lock:
+            global Session
+            sess = Session()
+            for message in messages:
+                new_message = Message(
+                    content = message['content'],
+                    timestamp= message['timestamp'],
+                    message_id = message['message_id'],
+                    author_id = message['author_id'],
+                    server_id = message['server_id'],
+                    channel_id = message['channel_id']
+                )
+                sess.add(new_message)
+            print(
+                "=============================================COMMITTING CHANNEL X=================================================================================")
+            # print(messages)
+            print(len(messages))
+
+            # try:
+            sess.commit()
+            # except Exception as e:
+            Session.remove()
+
+    def read_messages(self):
+        global Session
+        sess = Session()
+        messages = []
+        for message in sess.query(Message):
+            messages.append(message)
+            message.author
+            message.channel
+            message.server
+
+        Session.remove()
+        return messages
+
+
+    def create_user(self, discord_id, name):
+        """
+
+        :param content:
+        :param message_id:
+        :param author_id:
+        :param server_id:
+        :param channel_id:
+        :return:
+        """
+        global Session
+        sess = Session()
+
+        new_user = User(
+            discord_id=discord_id,
+            name=name
+        )
+        sess.add(new_user)
+        sess.commit()
+
+        Session.remove()
+
+    def read_users(self):
+        """
+
+        :return:
+        """
+        global Session
+        sess = Session()
+        users = {}
+        for id, name in sess.query(User.discord_id, User.name):
+            users[id] = name
+        Session.remove()
+        return users
+
+
+    def create_server(self, discord_id, name):
+        """
+
+        :param content:
+        :param message_id:
+        :param author_id:
+        :param server_id:
+        :param channel_id:
+        :return:
+        """
+        global Session
+        sess = Session()
+
+        new_server = Server(
+            discord_id=discord_id,
+            name=name
+        )
+        sess.add(new_server)
+        sess.commit()
+
+        Session.remove()
+
+    def read_servers(self):
+        """
+
+        :return:
+        """
+
+        global Session
+        sess = Session()
+        servers = {}
+        for id, name in sess.query(Server.discord_id, Server.name):
+            servers[id] = name
+        Session.remove()
+        return servers
+
+    def create_channel(self, discord_id, name):
+        """
+
+        :param content:
+        :param message_id:
+        :param author_id:
+        :param server_id:
+        :param channel_id:
+        :return:
+        """
+        global Session
+        sess = Session()
+
+        new_channel = Channel(
+            discord_id=discord_id,
+            name=name
+        )
+        sess.add(new_channel)
+        sess.commit()
+
+        Session.remove()
+
+    def read_channels(self):
+        """
+
+        :return:
+        """
+        global Session
+        sess = Session()
+        channels = {}
+        for id, name in sess.query(Channel.discord_id, Channel.name):
+            channels[id] = name
+        Session.remove()
+        return channels
